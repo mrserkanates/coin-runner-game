e030aa2a116e029ddaef012b1672b24b7d361f6d
Serkan <serkanates345@gmail.com>
2022-12-02 01:53:17 +0300

    Update4

--- Assets/Scripts/CoinMovement.cs
+++ Assets/Scripts/CoinMovement.cs
@@ -5,25 +5,41 @@ using UnityEngine;
 public class CoinMovement : MonoBehaviour
 {
     private Transform connectedCoin;
+    private float radius = 0.3f;
+    private float followSpeed = 30f;
 
     public Transform ConnectedCoin { get => connectedCoin; set => connectedCoin = value; }
+
+    void Start(){
+        //radius = GetComponent<MeshCollider>().bounds.size.x / 2;
+    }
 
     void Update()
     {
         float zDelta;
-        //zDelta = Mathf.Lerp(transform.position.z, ConnectedCoin.position.z, Time.deltaTime * 20);
-        float zDiff = Mathf.Abs(ConnectedCoin.position.z - transform.position.z);
-        if (ConnectedCoin.position.z > transform.position.z){
-            zDelta = Mathf.Lerp(transform.position.z, transform.position.z + zDiff * 10, Time.deltaTime * 20);
-        }else{
-            zDelta = Mathf.Lerp(transform.position.z, transform.position.z - zDiff * 10, Time.deltaTime * 20);
-        }
-        Debug.Log("ZDIFF: " + zDiff);
-        transform.position = new Vector3(
-            connectedCoin.position.x - 1f,
-            ConnectedCoin.position.y,
-            zDelta);
+        float targetZ;
 
+        if (ConnectedCoin.position.z > transform.position.z){ // go to left
+            targetZ = ConnectedCoin.position.z - 2 * radius * Mathf.Sin(Mathf.Deg2Rad * (180 - ConnectedCoin.eulerAngles.y));
+        }else if(ConnectedCoin.position.z < transform.position.z){ // go to right
+            targetZ = ConnectedCoin.position.z + 2 * radius * Mathf.Sin(Mathf.Deg2Rad * (ConnectedCoin.eulerAngles.y - 180));
+        }else{
+            targetZ = ConnectedCoin.position.z;
+        }
+
+        if (targetZ - transform.position.z > 0.02f){
+            zDelta = Mathf.Lerp(transform.position.z,
+            targetZ,
+            Time.deltaTime * followSpeed);
+        }else{
+            zDelta = targetZ;
+        }
+        Debug.Log("diff: " + (targetZ - transform.position.z));
+
+        //Debug.Log("con coin rot: " + ConnectedCoin.eulerAngles + "rad: " + radius + " | name: " + gameObject.name);
+        transform.position = new Vector3(connectedCoin.position.x - radius * 2f, ConnectedCoin.position.y, zDelta);
         transform.rotation = Quaternion.RotateTowards(transform.rotation, ConnectedCoin.rotation, 720 * Time.deltaTime);
+        transform.localScale = Vector3.Lerp(transform.localScale, ConnectedCoin.localScale, Time.deltaTime * 60);
     }
+    
 }
--- Assets/Scripts/MainCoin.cs
+++ Assets/Scripts/MainCoin.cs
@@ -7,12 +7,16 @@ public class MainCoin : MonoBehaviour
 {
     [SerializeField] private float verticalSpeed;
     [SerializeField] private float horizontalSpeed;
+    [SerializeField] private float scalingIncrease;
     [SerializeField] private Material platformMaterial;
     private bool isMoving = false;
     private List<Coin> collectedCoins;
+    private bool isScaling = false;
+    
 
     private void Awake() {
         collectedCoins = new List<Coin>();
+
     }
 
     private void OnEnable() {
@@ -28,11 +32,15 @@ public class MainCoin : MonoBehaviour
         platformMaterial.SetVector("_Pos", transform.position);
         if (isMoving){
             float horizontalInput = Input.GetAxis("Horizontal");
-            Vector3 movementDirection = new Vector3(verticalSpeed * Time.deltaTime, 0f, -horizontalInput * horizontalSpeed * Time.deltaTime);
+            Vector3 movementDirection = movementDirection = new Vector3(verticalSpeed * Time.deltaTime, 0f, -horizontalInput * horizontalSpeed * Time.deltaTime);
             transform.Translate(movementDirection, Space.World);
             if (movementDirection != Vector3.zero){
                 movementDirection = new Vector3(movementDirection.z, 0f, -movementDirection.x);
-                Quaternion toRotation = Quaternion.LookRotation(movementDirection, Vector3.down);
+                Quaternion toRotation = Quaternion.LookRotation(movementDirection, Vector3.up);
+                transform.rotation = Quaternion.RotateTowards(transform.rotation, toRotation, 720 * Time.deltaTime);
+            }else{
+                movementDirection = new Vector3(0f, 0f, -movementDirection.x);
+                Quaternion toRotation = Quaternion.LookRotation(movementDirection, Vector3.up);
                 transform.rotation = Quaternion.RotateTowards(transform.rotation, toRotation, 720 * Time.deltaTime);
             }
         }
@@ -46,11 +54,35 @@ public class MainCoin : MonoBehaviour
     private void OnTriggerEnter(Collider other) {
         if (other.gameObject.tag == "Coin"){
             collectedCoins.Add(other.gameObject.GetComponent<Coin>());
+            other.gameObject.GetComponent<Collider>().isTrigger = false;
             other.gameObject.AddComponent<CoinMovement>();
             if (collectedCoins.Count == 1)
                 other.gameObject.GetComponent<CoinMovement>().ConnectedCoin = transform;
             else
                 other.gameObject.GetComponent<CoinMovement>().ConnectedCoin = collectedCoins[collectedCoins.Count - 2].transform;
+            if (isScaling){
+                StopCoroutine(ScaleUpAndDown());
+                isScaling = false;
+            }
+            StartCoroutine(ScaleUpAndDown());
         }
+    }
+
+    private IEnumerator ScaleUpAndDown(){
+        isScaling = true;
+        float newScale = 200;
+        transform.localScale = new Vector3(newScale, newScale, newScale);
+        while (newScale < 200 + scalingIncrease && isScaling){
+            newScale += 1;
+            transform.localScale = new Vector3(newScale, newScale, newScale);
+            yield return new WaitForSeconds(0.001f);
+        }
+
+        while (newScale > 200 && isScaling){
+            newScale -= 1;
+            transform.localScale = new Vector3(newScale, newScale, newScale);
+            yield return new WaitForSeconds(0.001f);
+        }
+        isScaling = false;
     }
 }
